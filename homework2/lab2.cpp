309a581de97d16389caf477953fcbbda5c361757
Matthew McLeod <matthew.mcleod@mavs.uta.edu>
2018-07-10 21:05:14 -0500

    getting rid of obtrusive comments

--- homework2/lab2.cpp
+++ homework2/lab2.cpp
@@ -1,47 +1,16 @@
 //
 //    Copyright 2018 Christopher D. McMurrough
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-
-/*******************************************************************************************************************//**
- * @file cv_ellipse.cpp
- * @brief C++ example of Canny edge detection and ellipse model fitting in OpenCV
- * @author Christopher D. McMurrough
- **********************************************************************************************************************/
-
-// include necessary dependencies
 #include <iostream>
 #include <string>
 #include "opencv2/opencv.hpp"
 
-// configuration parameters
 #define NUM_COMNMAND_LINE_ARGUMENTS 1
 
-/*******************************************************************************************************************//**
- * @brief program entry point
- * @param[in] argc number of command line arguments
- * @param[in] argv string array of command line arguments
- * @return return code (0 for normal termination)
- * @author Christoper D. McMurrough
- **********************************************************************************************************************/
 int main(int argc, char **argv)
 {
     cv::Mat imageIn;
     double model[5];
 
-    // validate and parse the command line arguments
     if(argc != NUM_COMNMAND_LINE_ARGUMENTS + 2)
     {
         std::printf("USAGE: %s <image_path> <model>\n", argv[0]);
@@ -51,7 +20,6 @@ int main(int argc, char **argv)
     {
         imageIn = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
-        // check for file error
         if(!imageIn.data)
         {
             std::cout << "Error while opening file " << argv[1] << std::endl;
@@ -73,35 +41,28 @@ int main(int argc, char **argv)
         }
     }
 
-    // get the image size
     std::cout << "image width: " << imageIn.size().width << std::endl;
     std::cout << "image height: " << imageIn.size().height << std::endl;
     std::cout << "image channels: " << imageIn.channels() << std::endl;
 
-    // convert the image to grayscale
     cv::Mat imageGray;
     cv::cvtColor(imageIn, imageGray, cv::COLOR_BGR2GRAY);
 
-    // normalize image
     cv::Mat imageNormalized;
     cv::normalize(imageGray, imageNormalized, 0, 255);
 
-    // equalize image
     cv::Mat imageEqualized;
     cv::equalizeHist(imageNormalized, imageEqualized);
 
-    // find the image edges
     cv::Mat imageEdges;
     const double cannyThreshold1 = 100;
     const double cannyThreshold2 = 200;
     const int cannyAperture = 3;
     cv::Canny(imageGray, imageEdges, cannyThreshold1, cannyThreshold2, cannyAperture);
 
-    // locate the image contours (after applying a threshold or canny)
     std::vector<std::vector<cv::Point> > contours;
     cv::findContours(imageEdges, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
 
-    // draw the contours
     cv::Mat imageContours = cv::Mat::zeros(imageEdges.size(), CV_8UC3);
     cv::RNG rand(12345);
     for(int i = 0; i < contours.size(); i++)
@@ -110,15 +71,12 @@ int main(int argc, char **argv)
         cv::drawContours(imageContours, contours, i, color);
     }
 
-    // compute minimum area bounding rectangles
     std::vector<cv::RotatedRect> minAreaRectangles(contours.size());
     for(int i = 0; i < contours.size(); i++)
     {
-        // compute a minimum area bounding rectangle for the contour
         minAreaRectangles[i] = cv::minAreaRect(contours[i]);
     }
     
-    // draw the rectangles
     cv::Mat imageRectangles = cv::Mat::zeros(imageEdges.size(), CV_8UC3);
     for(int i = 0; i < contours.size(); i++)
     {
@@ -131,18 +89,15 @@ int main(int argc, char **argv)
         }
     }
 
-    // fit ellipses to contours containing sufficient inliers
     std::vector<cv::RotatedRect> fittedEllipses(contours.size());
     for(int i = 0; i < contours.size(); i++)
     {
-        // compute an ellipse only if the contour has more than 5 points (the minimum for ellipse fitting)
         if(contours.at(i).size() > 500)
         {
             fittedEllipses[i] = cv::fitEllipse(contours[i]);
         }
     }
 
-    // eliminate ellipses that are too big
     std::vector<cv::RotatedRect> normalEllipses;
     for(int i = 0; i < fittedEllipses.size(); i++) {
         if(fittedEllipses[i].size.height < 1000 && fittedEllipses[i].size.width < 1000) {
@@ -151,7 +106,6 @@ int main(int argc, char **argv)
         }
     }  
 
-    // eliminate ellipses that are contained within other ellipses
     std::vector<cv::RotatedRect> coinEllipses;
     for(int i = 0; i < normalEllipses.size(); i++) {
         bool isInsideOtherEllipse = false;
@@ -159,7 +113,6 @@ int main(int argc, char **argv)
         for(int j = 0; j < normalEllipses.size(); j++) {
             if (i == j) continue;
 
-            // points are stored bottomLeft, topLeft, topRight, bottomRight
             cv::Point2f pts[4];
             normalEllipses[j].points(pts);
             if(((center.x > pts[0].x && center.y < pts[0].y) 
@@ -175,7 +128,6 @@ int main(int argc, char **argv)
         if(!isInsideOtherEllipse) coinEllipses.push_back(normalEllipses[i]);
     }
 
-    // determining the diameter of each ellipse
     std::vector<double> ellipseDiameters;
     for(int i = 0; i < coinEllipses.size(); i++) {
         cv::Point2f pts[4];
@@ -185,11 +137,6 @@ int main(int argc, char **argv)
         ellipseDiameters.push_back(euclideanDistance);
     }
 
-    // classifying each coin type by diameter
-    // you could make this code camera distance agnostic by using a segmentation algorithm
-    // a couple to consider would be fishers natural breaks or kde, but thats another class
-    // this piece requires a model.txt file with predefined diameters for each cointype
-    
     enum CoinType {penny, nickel, dime, quarter};
     int coinCount[4] = {0,0,0,0};
     int ellipseAssignments[ellipseDiameters.size()];
@@ -206,7 +153,6 @@ int main(int argc, char **argv)
         coinCount[ellipseAssignments[i]]++;
     }
 
-    // add up the change
     double total = 0;
     for(int coinInt = penny; coinInt != quarter+1; coinInt++) {
         switch(static_cast<CoinType>(coinInt)) {
@@ -232,7 +178,6 @@ int main(int argc, char **argv)
     }
     std::cout << "There is $" << total << " shown in the image!" << std::endl;
 
-    // draw the ellipses
     cv::Mat imageEllipse = cv::Mat::zeros(imageEdges.size(), CV_8UC3);
     for(int i = 0; i < coinEllipses.size(); i++)
     {
@@ -259,7 +204,6 @@ int main(int argc, char **argv)
         }
     }
 
-    // display the images
     cv::imshow("imageIn", imageIn);
     cv::imshow("imageGray", imageGray);
     cv::imshow("imageEdges", imageEdges);
