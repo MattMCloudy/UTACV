4e7e5df2f7bf634b8e4c8e76f86bebfa7e813f9f
Matthew McLeod <matthew.mcleod@mavs.uta.edu>
2018-08-12 23:53:13 -0500

    the more you learn about cpp

--- homework4/homework4.cpp
+++ homework4/homework4.cpp
@@ -559,7 +559,7 @@ int main(int argc, char** argv)
                         switch(cloudFiltered->points.at(clusterIndices.at(i).indices.at(50)).b) {
                             case 255:
                                 // Logic for Blue Spheres
-                                pcl::PointXYZRGBA* top_of_sphere = getTopOfSphere(cloudFiltered, clusterIndices.at(i)::Ptr, plane_z_val);
+                                pcl::PointXYZRGBA* top_of_sphere = getTopOfSphere(cloudFiltered, clusterIndices.at(i).Ptr, plane_z_val);
                                 break;
                         }
                         break;
@@ -567,7 +567,7 @@ int main(int argc, char** argv)
                         switch(cloudFiltered->points.at(clusterIndices.at(i).indices.at(50)).b) {
                             case 0:
                                 // Logic for Green Boxes
-                                pcl::PointXYZRGBA* top_of_sphere = getTopOfSphere(cloudFiltered, clusterIndices.at(i)::Ptr, plane_z_val);
+                                pcl::PointXYZRGBA* top_of_sphere = getTopOfSphere(cloudFiltered, clusterIndices.at(i).Ptr, plane_z_val);
                                 break;
                         }
                         break;
